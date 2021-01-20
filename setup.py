864274cf6b30560da8ff7ab3246c3b226374e2ef
Guillaume Gaullier <guillaume@gaullier.org>
2021-01-20 12:15:19 +0100

    Version 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding = 'utf-8') as f:
 
 setup(
     name = 'topaztrainmetrics',
-    version = '1.0',
+    version = '1.1',
 
     description = 'Plot metrics from a Topaz training run',
     long_description = readme,
