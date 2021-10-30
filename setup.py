effe129c81efeaf0ded9c011c856bd9bdf330cba
Guillaume Gaullier <guillaume@gaullier.org>
2021-10-30 19:55:38 +0200

    Updated dependency list

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding = 'utf-8') as f:
 
 setup(
     name = 'topaztrainmetrics',
-    version = '1.1',
+    version = '1.2',
 
     description = 'Plot metrics from a Topaz training run',
     long_description = readme,
@@ -34,11 +34,11 @@ setup(
 
     py_modules = ["topaztrainmetrics"],
 
-    python_requires = '>=3.9.1',
+    python_requires = '>=3.9.7',
     install_requires = [
-        'click>=7.1.2',
-        'matplotlib>=3.3.2',
-        'pandas>=1.1.5'
+        'click>=8.0.3',
+        'matplotlib>=3.4.3',
+        'pandas>=1.3.3'
     ],
 
     entry_points = {
